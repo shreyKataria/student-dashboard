ac1c7053dc5fd7a3e12bc348ae5ef19032b157fa
shrey kataria <shreykataria1@gmail.com>
2024-05-29 16:33:17 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ const express = require("express");
 const connectDB = require("./config/db");
 const router = require("./routes/studentRoute");
 const app = express();
+var cors = require("cors");
 
 const port = process.env.PORT || 8000;
 
 // middleware
 app.use(express.json());
+app.use(cors());
 
 // routes
 app.use("/api", router);
